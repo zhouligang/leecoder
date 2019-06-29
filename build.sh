905cb49fb985c37f58f692511feed2a7d84067b7
leecoder <zhouligang2006@163.com>
2019-06-29 21:44:01 +0800

    更新脚本

--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@
 #
 #echo "Halo version: $VERSION"
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build --build-arg JAR_FILE="./target/halo.jar" -t $DOCKER_USERNAME/halo .
-docker tag $DOCKER_USERNAME/halo $DOCKER_USERNAME/halo:latest
-docker push $DOCKER_USERNAME/halo:latest
+#docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+#docker build -f Dockerfile .
+#docker build --build-arg JAR_FILE="./target/halo.jar" -t $DOCKER_USERNAME/halo .
+docker build -t leecoder .
+docker run -d -p 9000:9000 leecoder
+#docker tag $DOCKER_USERNAME/halo $DOCKER_USERNAME/halo:latest
+#docker push $DOCKER_USERNAME/halo:latest
