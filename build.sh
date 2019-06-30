7408728b5852a116090a74f7918047b05ef401be
leecoder <zhouligang2006@163.com>
2019-06-30 16:04:59 +0800

    增加推送镜像

--- build.sh
+++ build.sh
@@ -17,8 +17,8 @@ docker run --rm -it -d --name $CONTAINER_NAME -p 9000:9000  -v ~/.leecoder:/root
 # 删除没有版本号的镜像
 docker images|grep "<none>"|awk '{print $3}'|xargs docker rmi
 
-#docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 #docker build -f Dockerfile .
 #docker build --build-arg JAR_FILE="./target/halo.jar" -t $DOCKER_USERNAME/halo .
 #docker tag leecoder $DOCKER_USERNAME/leecoder:latest
-#docker push $DOCKER_USERNAME/leecoder:latest
+docker push $DOCKER_USERNAME/leecoder:latest
