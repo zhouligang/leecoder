34976306fa5904b58513cc292819208cced608ae
leecoder <zhouligang2006@163.com>
2019-06-30 13:28:29 +0800

    测试github hook

--- build.sh
+++ build.sh
@@ -3,11 +3,10 @@
 #VERSION=$(ls build/libs | sed 's/.*halo-//' | sed 's/.jar$//')
 #
 #echo "Halo version: $VERSION"
-
+docker build -t leecoder .
+docker run --rm -it -d --name leecoder -p 9000:9000  -v ~/.leecoder:/root/.leecoder leecoder
 #docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 #docker build -f Dockerfile .
 #docker build --build-arg JAR_FILE="./target/halo.jar" -t $DOCKER_USERNAME/halo .
-docker build -t leecoder .
-docker run --rm -it -d --name leecoder -p 9000:9000  -v ~/.leecoder:/root/.leecoder leecoder
-#docker tag $DOCKER_USERNAME/halo $DOCKER_USERNAME/halo:latest
-#docker push $DOCKER_USERNAME/halo:latest
+#docker tag leecoder $DOCKER_USERNAME/leecoder:latest
+#docker push $DOCKER_USERNAME/leecoder:latest
