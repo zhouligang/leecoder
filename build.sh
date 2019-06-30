c1931a8642a9ae6990f2bc79d8ed733f2eb3422f
leecoder <zhouligang2006@163.com>
2019-06-30 14:46:19 +0800

    增加容器删除

--- build.sh
+++ build.sh
@@ -3,8 +3,20 @@
 #VERSION=$(ls build/libs | sed 's/.*halo-//' | sed 's/.jar$//')
 #
 #echo "Halo version: $VERSION"
-docker build -t leecoder .
-docker run --rm -it -d --name leecoder -p 9000:9000  -v ~/.leecoder:/root/.leecoder leecoder
+CONTAINER_NAME="leecoder"
+docker build -t $CONTAINER_NAME .
+cid=$(docker ps -a| grep "$CONTAINER_NAME" | awk '{print $1}')
+if [ "$cid" != "" ]; then
+    echo "--------正在删除容器，ID为：$cid------------------------------"
+#   docker stop $cid
+   docker rm -f $cid
+fi
+docker run --rm -it -d --name $CONTAINER_NAME -p 9000:9000  -v ~/.leecoder:/root/.leecoder $CONTAINER_NAME
+# 删除指定名称的镜像
+#docker images|grep "$CONTAINER_NAME"|awk '{print $3}'|xargs docker rmi
+# 删除没有版本号的镜像
+docker images|grep "<none>"|awk '{print $3}'|xargs docker rmi
+
 #docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 #docker build -f Dockerfile .
 #docker build --build-arg JAR_FILE="./target/halo.jar" -t $DOCKER_USERNAME/halo .
