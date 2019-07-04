4688fa580efcfac1ef63107a0b67e71e74f29dfb
周黎钢 <zhouligang2006@163.com>
2019-07-04 15:58:30 +0800

    更新脚本
    Change-Id: Iee0041110fd7a205ab3fe67de1fe0c71817550dd

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ fi
 echo "--------删除镜像成功-------------------------------------"
 
 #推送镜像到官方仓库
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+winpty docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 #docker build -f Dockerfile .
 #docker build --build-arg JAR_FILE="./target/halo.jar" -t $DOCKER_USERNAME/halo .
 #推送只需打开下面的注释
